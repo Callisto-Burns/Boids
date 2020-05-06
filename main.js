0168f3015bc3378865c781fa613cb4ef66f06d9b
Jackson Burns <jc3urns@gmail.com>
2020-05-06 19:31:21 -0400

    made a window

---
+++ main.js
@@ -0,0 +1,31 @@
+global.__basedir = __dirname
+
+const electron = require('electron')
+const url = require('url')
+const path = require('path')
+
+const {app, BrowserWindow} = electron
+
+let mainWindow
+
+app.on('ready', ()=>{
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        resizeable: false
+    })
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__basedir, "src/index.html"),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    mainWindow.on('closed', () => {
+        app.quit()
+    })
+})
+
