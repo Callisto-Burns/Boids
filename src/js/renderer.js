0137debad0058999d3a23f9f4749f139f6a06869
Jackson Burns <jc3urns@gmail.com>
2020-05-08 22:06:31 -0400

    no

--- src/js/renderer.js
+++ src/js/renderer.js
@@ -1,10 +1,6 @@
 const THREE = require('three')
-<<<<<<< HEAD
-const Boid = require('./js/Boid')
-let world, camera, renderer
-=======
+const Boid = require('Boid')
 let world, camera, renderer, plane
->>>>>>> 4869e8389cb383379d1d529dddae147030fb251b
 
 init()
 animate()
@@ -36,12 +32,12 @@ function initScene() {
     world.add(axesHelper)
     var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
     world.add(directionalLight)
-<<<<<<< HEAD
+    var gridHelper = new THREE.GridHelper(1000,1000)
+    world.add(gridHelper)
     
     var boid = new Boid(world, 0, 0)
     world.add(boid)
 
-=======
 
     plane = new THREE.Mesh(
         new THREE.PlaneGeometry(100,100,100,100),
@@ -70,5 +66,4 @@ function initRenderer(){
     })
     renderer.setSize(window.innerWidth, window.innerHeight)
     document.body.appendChild(renderer.domElement)
->>>>>>> 4869e8389cb383379d1d529dddae147030fb251b
 }
