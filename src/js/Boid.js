15093e5e4d9b3ba3d127f17a387222fadb822ba2
RHenderzon <rrh3@email.sc.edu>
2020-05-20 08:36:09 -0400

    Added some code for rules still need to fix it

--- src/js/Boid.js
+++ src/js/Boid.js
@@ -1,6 +1,8 @@
 const THREE = require('three')
+
+// general constants
 let num_rays = 10, sight_radius = 10, body_radius = 5, body_height = 10, 
-    radial_segment = 20, height_segment = 2, v_scalar = 10, sphere_segment = 16
+    radial_segment = 20, height_segment = 2, v_scalar = 1, sphere_segment = 16
 
 var Boid = function(...args){
     THREE.Object3D.apply(this, [])
@@ -28,28 +30,112 @@ var Boid = function(...args){
     this.sphere.visible = false;
     this.scene.add(this.sphere)
 
-    // intitialize nearbyoids array as empty
+    // initialize nearbyoids array as empty
     this.nearbyoids = []
 
-    // intitialize random velocity
+    // initialize random velocity
     this.velocity = new THREE.Vector3(
         Math.random() * v_scalar - v_scalar / 2, 
         Math.random() * v_scalar - v_scalar / 2, 
         0
     )
     this.orientVector()
+
+    // initialize a zero Vector3 difference vector to be used in updating
+    this.dif_vector = new THREE.Vector3(0,0,0)
 }
 
 Boid.prototype = Object.create(THREE.Object3D.prototype)
 
 Boid.prototype.constructor = Boid
 
-Boid.prototype.update = function(delta){
+/** constants for updating velocities and positions **/
+let d_v_scalar = .001, p_v_scalar = .001, personal_space = .01
+
+// if boid is too close to nearbyoids, then move it back to the limit of the distance, return a difference vector
+Boid.prototype.rule1 = function(){
+
+    var ret_vec = new THREE.Vector3(0,0,0)
+    for (i = 0; i < this.nearbyoids.length; i++) {
+        if (Math.abs(this.nearbyoids[i].mesh.position.distanceTo(this.mesh.position)) < personal_space){
+            ret_vec.add(
+                new THREE.Vector3(this.mesh.position.x, this.mesh.position.y, this.mesh.position.z).addScaledVector(this.nearbyoids[i].mesh.position, -1)
+                )
+        }
+    }
+
+    return ret_vec
+}
+
+// averages difference of velocity to nearbyoids velocities, returning a difference vector 
+Boid.prototype.rule2 = function(){
+
+    var ret_vec = new THREE.Vector3(0,0,0)
+    for (i = 0; i < this.nearbyoids.length; i++){
+        ret_vec.add(this.nearbyoids[i].velocity.addScaledVector(this.velocity, -1))
+    }
+    ret_vec.divideScalar(this.nearbyoids.length * d_v_scalar)
+
+    return ret_vec
+}
+
+// moves boid closer to overall average position, return a difference vector
+Boid.prototype.rule3 = function(){
+
+    if (this.nearbyoids.length == 0){
+        return new THREE.Vector3(0,0,0)
+    }
+    var ret_vec = new THREE.Vector3(0,0,0)
+    for (i = 0; i < this.nearbyoids.length; i++){
+        ret_vec.add(this.nearbyoids[i].mesh.position)
+    }
+    ret_vec.divideScalar(this.nearbyoids.length)
+    ret_vec.addScaledVector(this.mesh.position, -1)
+    ret_vec.multiplyScalar(p_v_scalar)
+    
+    return ret_vec
+}
+
+Boid.prototype.differenceVector = function(){
+    // create the difference vector as zero vector to be added to
+    dif_vec = new THREE.Vector3(0,0,0)
+
+    // add velocity changing rules to the difference vector
+    dif_vec.add(this.rule1())
+    dif_vec.add(this.rule2())
+    dif_vec.add(this.rule3())
+
+    return dif_vec
+}
+
+// should be called before finalUpdate, allows multiple boids to update positions simultaneously
+Boid.prototype.readyUpdate = function() {
+    this.dif_vector = this.differenceVector()
+}
+
+// actually updates the positions and velocity
+Boid.prototype.finalUpdate = function(delta){
+    // update the velocity
+    this.velocity = this.velocity.add(this.dif_vector)
+
+    // update the position
     this.mesh.position.addScaledVector(this.velocity, delta * v_scalar)
+
+    // update the orientation
+    this.orientVector()
+
 }
 
 Boid.prototype.orientVector = function(){
     this.mesh.rotation.z = Math.atan2(this.velocity.y, this.velocity.x) - Math.PI / 2
+}
+
+Boid.prototype.addBoid = function(boid){
+    this.nearbyoids.push(boid)
+}
+
+Boid.prototype.setVelocity = function(vel){
+    this.velocity.copy(vel)
 }
 
 module.exports = Boid
--- src/js/renderer.js
+++ src/js/renderer.js
@@ -27,7 +27,13 @@ function animate() {
 function render() {
     var delta = clock.getDelta()
     renderer.render(world, camera)
-    boid.update(delta)
+
+    boid1.readyUpdate()
+    boid2.readyUpdate()
+
+    boid1.finalUpdate(delta)
+    boid2.finalUpdate(delta)
+
     controls.update(delta)
 }
 
@@ -41,8 +47,22 @@ function initScene() {
     var gridHelper = new THREE.GridHelper(10000,10000)
     world.add(gridHelper)
     
-    boid = new Boid(world, 0, 0, 0)
-    world.add(boid)
+    boid1 = new Boid(world, 0, 0, 0)
+    world.add(boid1)
+
+    boid2 = new Boid(world, 10, 0, 0)
+    world.add(boid2)
+
+    boid1.addBoid(boid2)
+    boid2.addBoid(boid1)
+    
+    vel1 = new THREE.Vector3(1,1,0)
+    vel2 = new THREE.Vector3(-1,1,0)
+
+    boid1.setVelocity(vel1)
+    boid1.orientVector()
+    boid2.setVelocity(vel2)
+    boid2.orientVector()
 }
 
 function initCamera(){
